3b0c9d58b2cb3c087c534045918ec279efe99ffc
mercuriver <mercuriver@gmail.com>
2021-08-22 22:02:08 +0900

    feat: 기본 동작 구현

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import App from './app.js';
+// import App from './app.js';
+// App(document.querySelector('#app'));
 
-App(document.querySelector('.app'));
+const $app = document.querySelector('#app');
+
+let state = {
+  items: ['item1', 'item2', 'item3', 'item4'],
+};
+
+const render = () => {
+  const { items } = state;
+  $app.innerHTML = `
+    <ul>
+      ${items.map((item) => `<li>${item}</li>`).join('')}
+    </ul>
+    <button id="append">추가</button>
+  `;
+  document.querySelector('#append').addEventListener('click', () => {
+    setState({
+      items: [...items, `item${items.length + 1}`],
+    });
+  });
+};
+
+const setState = (newState) => {
+  state = { ...state, ...newState };
+  render();
+};
+
+render();
