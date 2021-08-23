43d93b923dc985e92c381eb0f1a11e213f4e045f
mercuriver <mercuriver@gmail.com>
2021-08-23 21:33:37 +0900

    refactor: 모듈화

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-export default function App(app) {}
+import Items from './components/Items.js';
+
+class App {
+  constructor() {
+    const $app = document.querySelector('#app');
+    new Items($app);
+  }
+}
+
+new App();
---
+++ src/components/Items.js
@@ -0,0 +1,26 @@
+import Component from '../core/Component.js';
+
+class Items extends Component {
+  setup() {
+    this.$state = { items: ['item1', 'item2', 'item3', 'item4'] };
+  }
+
+  template() {
+    const { items } = this.$state;
+    return `
+      <ul>
+        ${items.map((item) => `<li>${item}</li>`).join('')}
+      </ul>
+      <button>추가</button>
+    `;
+  }
+
+  setEvent() {
+    this.$target.querySelector('button').addEventListener('click', () => {
+      const { items } = this.$state;
+      this.setState({ items: [...items, `item${items.length + 1}`] });
+    });
+  }
+}
+
+export default Items;
