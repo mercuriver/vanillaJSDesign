251e9367f508d95b446f3583a6e341c319e28924
mercuriver <mercuriver@gmail.com>
2021-08-31 00:35:28 +0900

    refactor: proxy를 사용한 observer 코드 개선

--- src/core/observer.js
+++ src/core/observer.js
@@ -15,25 +15,44 @@ const observe = (fn) => {
 };
 
 const observable = (obj) => {
-  Object.keys(obj).forEach((key) => {
-    let _value = obj[key];
-    const observers = new Set();
+  const observerMap = {};
 
-    Object.defineProperty(obj, key, {
-      get() {
-        if (currentObserver) observers.add(currentObserver);
-        return _value;
-      },
-      set(value) {
-        if (_value === value) return;
-        // Todo: Set, Map, WeekSet, WekkMap은 JSON.stringify로 변환되지 않음.
-        if (JSON.stringify(_value) === JSON.stringify(value)) return;
-        _value = value;
-        observers.forEach((fn) => fn());
-      },
-    });
+  return new Proxy(obj, {
+    get(target, name) {
+      observerMap[name] = observerMap[name] || new Set();
+      if (currentObserver) observerMap[name].add(currentObserver);
+      return target[name];
+    },
+    set(target, name, value) {
+      if (target[name] === value) return true;
+      if (JSON.stringify(target[name]) === JSON.stringify(value)) return true;
+      target[name] = value;
+      observerMap[name].forEach((fn) => fn());
+      return true;
+    },
   });
-  return obj;
 };
+
+// const observable = (obj) => {
+//   Object.keys(obj).forEach((key) => {
+//     let _value = obj[key];
+//     const observers = new Set();
+
+//     Object.defineProperty(obj, key, {
+//       get() {
+//         if (currentObserver) observers.add(currentObserver);
+//         return _value;
+//       },
+//       set(value) {
+//         if (_value === value) return;
+//         // Todo: Set, Map, WeekSet, WekkMap은 JSON.stringify로 변환되지 않음.
+//         if (JSON.stringify(_value) === JSON.stringify(value)) return;
+//         _value = value;
+//         observers.forEach((fn) => fn());
+//       },
+//     });
+//   });
+//   return obj;
+// };
 
 export { observe, observable };
