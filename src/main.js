554c63a7f61bf29f5edb2b922b0878230a56162b
mercuriver <mercuriver@gmail.com>
2021-09-11 21:11:00 +0900

    feat: 실제 DOM 생성을 위한 createElement 작성

--- src/main.js
+++ src/main.js
@@ -3,12 +3,33 @@ function h(type, props, ...children) {
   return { type, props, children: children.flat() };
 }
 
+function createElement(node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
+
+  const $el = document.createElement(node.type);
+
+  Object.entries(node.props || {})
+    .filter(([attr, value]) => value)
+    .forEach(([attr, value]) => $el.setAttribute(attr, value));
+
+  try {
+    node.children.map(createElement).forEach((child) => $el.appendChild(child));
+  } catch (e) {
+    console.log(node);
+    console.error(e);
+  }
+
+  return $el;
+}
+
 const state = [
   { id: 1, completed: false, content: 'todo list item 1' },
   { id: 2, completed: true, content: 'todo list item 2' },
 ];
 
-const vm = (
+const virtualDom = (
   <div id="app">
     <ul>
       {state.map(({ completed, content }) => (
@@ -26,8 +47,7 @@ const vm = (
   </div>
 );
 
-const $root = document.body.querySelector('#root');
+const realDom = createElement(virtualDom);
 
-$root.innerHTML = `
-  <pre>${JSON.stringify(vm, null, 2)}</pre>
-`;
+const $root = document.body.querySelector('#root');
+$root.appendChild(realDom);
